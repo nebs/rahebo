64413039039457b8272665b0508e9661c6b3fa1f
Nebojsa Petrovic <nebs@causes.com>
2014-02-18 00:11:01 -0800

    Add base script, gitignore and Gemfile
    This commit adds a basic script that initializes the rails app,
    initializes git, adds the gitignore and Gemfile from the server and runs
    bundle install.

---
+++ rahebo.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+project_name=$1
+
+# Initialize Rails project
+rails new $project_name
+cd $project_name
+
+# Initialize git
+git init
+curl -o .gitignore https://raw.github.com/nebspetrovic/rahebo/master/lib/rahebo.gitignore
+
+# Initialize Gemfile
+curl -o Gemfile https://raw.github.com/nebspetrovic/rahebo/master/lib/Gemfile
+
+# Setup bundle
+bundle install
+bundle exec rails g foundation:install
+bundle exec rails g rspec:install
+bundle exec rails g backbone:install
+
