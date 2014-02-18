1cba635045d399489e527cd10d762ace37876b06
Nebojsa Petrovic <nebs@causes.com>
2014-02-18 09:00:59 -0800

    Remove application layout before initializing foundation
    When initializing foundation the application layout gets overwritten.
    This commit updates the rahebo script to remove this file before hand to
    avoid the overwriting prompt.

--- rahebo.sh
+++ rahebo.sh
@@ -12,6 +12,9 @@ curl -o .gitignore https://raw.github.com/nebspetrovic/rahebo/master/lib/rahebo.
 # Initialize Gemfile
 curl -o Gemfile https://raw.github.com/nebspetrovic/rahebo/master/lib/Gemfile
 
+# Remove the application layout since it will get overwritten by foundation
+rm app/views/layouts/application.html.erb
+
 # Setup bundle
 bundle install
 bundle exec rails g foundation:install
@@ -19,7 +22,6 @@ bundle exec rails g rspec:install
 bundle exec rails g backbone:install
 
 # Replace the application layout with a haml version
-rm app/views/layouts/application.html.erb
 curl -o app/views/layouts/application.html.haml https://raw.github.com/nebspetrovic/rahebo/master/lib/application.html.haml
 
 echo "---- RAHEBO COMPLETED SUCCESSFULLY ----"
